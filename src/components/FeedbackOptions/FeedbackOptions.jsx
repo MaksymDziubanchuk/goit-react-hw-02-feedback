f23460a92aa1da26c18c2b7e8cfaab28afe0f8b4
MaksymDziubanchuk <94477065+MaksymDziubanchuk@users.noreply.github.com>
2022-07-19 23:57:11 +0300

    added some styles

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import css from 'components/FeedbackOptions/FeedbackOptions.module.css';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
-  <div onClick={onLeaveFeedback}>
-    <button onClick={options}>Good</button>
-    <button onClick={options}>Neutral</button>
-    <button onClick={options}>Bad</button>
+  <div>
+    <button
+      className={css.btn}
+      onClick={e => {
+        options(e);
+        onLeaveFeedback();
+      }}
+    >
+      Good
+    </button>
+    <button
+      className={css.btn}
+      onClick={e => {
+        options(e);
+        onLeaveFeedback();
+      }}
+    >
+      Neutral
+    </button>
+    <button
+      className={css.btn}
+      onClick={e => {
+        options(e);
+        onLeaveFeedback();
+      }}
+    >
+      Bad
+    </button>
   </div>
 );
 
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import css from 'components/Section/Section.module.css';
 
 export const Section = ({ title, children }) => {
   return (
-    <section>
+    <section className={css.section}>
       <h1>{title}</h1>
       {children}
     </section>
