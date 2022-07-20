d7515b57b6d4eb32f48b7aa4a0b73547020cbca9
MaksymDziubanchuk <94477065+MaksymDziubanchuk@users.noreply.github.com>
2022-07-20 20:53:41 +0300

    bugfix

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,37 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import css from 'components/FeedbackOptions/FeedbackOptions.module.css';
 
-export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
-  <div>
-    <button
-      className={css.btn}
-      onClick={e => {
-        options(e);
-        onLeaveFeedback();
-      }}
-    >
-      Good
-    </button>
-    <button
-      className={css.btn}
-      onClick={e => {
-        options(e);
-        onLeaveFeedback();
-      }}
-    >
-      Neutral
-    </button>
-    <button
-      className={css.btn}
-      onClick={e => {
-        options(e);
-        onLeaveFeedback();
-      }}
-    >
-      Bad
-    </button>
-  </div>
-);
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  const feedbackOptions = ['Good', 'Neutral', 'Bad'];
+  return (
+    <ul className={css.list}>
+      {feedbackOptions.map(item => (
+        <li key={item}>
+          <button
+            className={css.btn}
+            onClick={e => {
+              options(e);
+              onLeaveFeedback();
+            }}
+          >
+            {item}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 FeedbackOptions.propTypes = {
   options: PropTypes.elementType.isRequired,
